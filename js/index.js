e75d1771084600c6cfa4d9d2908632ef8074b524
subrotto <subrotokumargunopu@gmail.com>
2023-09-20 00:06:15 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,52 @@
+const APIkey='a7b2165a7a6c404a730e7e9c0590b7df';
+let localoy='';
+const search=()=>{
+
+    const cityField=document.getElementById('city-name');
+    const cityName=cityField.value;
+    cityField.value='';
+    localoy=cityName;
+
+    const url=`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${APIkey}`;
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(infos=>cityPosition(infos[0]));
+
+};
+
+const cityPosition=data=>{
+  
+    const lat=data.lat;
+    const lon=data.lon;
+
+
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>weatherResult(data));
+};
+
+
+const weatherResult=(data)=>{
+
+
+    const country=document.getElementById('country');
+    country.innerText='Country : '+data.sys.country;
+    const city=document.getElementById('cityName');
+    city.innerText='City Name : '+localoy;
+
+    const temp=document.getElementById('temp');
+    temp.innerText='Temperature :'+data.main.temp;
+    const atm=document.getElementById('atm');
+    atm.innerText='Atmosphere :'+data.weather[0].main;
+    
+    const feeltemp=document.getElementById('feelslike');
+    feeltemp.innerText='Feels like :'+data.main.feels_like;
+    const airspeed=document.getElementById('airspeed');
+    airspeed.innerText='Air Speed :'+data.wind.speed;
+    const degree=document.getElementById('degree');
+    degree.innerText='Wind Direction :'+data.wind.deg + ' Degree';
+
+}
